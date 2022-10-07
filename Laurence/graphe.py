4a8bba250df27ece27be6c6c94c9cf399ab62784
LaurenceAndraud <99392456+LaurenceAndraud@users.noreply.github.com>
2022-10-07 11:28:18 +0200

    Add files via upload

---
+++ Laurence/graphe.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#pour faire notre graphe, d'abord les indications sur l'axe des abscisses, puis les indications sur l'axe des ordonnées
+plt.plot([2015, 2016, 2017, 2018, 2019, 2020, 2021], [3.9152542372881354, 3.9753086419753085, 3.9245283018867925, 3.953488372093023, 3.933333333333333, None, None])
+
+plt.title('Evolution de la moyenne Time_signature sur le Top 50') #ajoute un titre à notre graphe
+plt.show() # affiche la figure à l'écran
